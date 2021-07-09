4d4605831629a8042442fcfc53a5a543485f2aa2
Aniket Tatte <tatteaniket1506@gmail.com>
2021-07-10 01:11:30 +0530

    Added functionality of Creating and Receiving Dynamic Links (Deep-linking) to the browser.

---
+++ legacy/src/main/java/jp/hazuki/yuzubrowser/legacy/utils/CreateDynamicLinks.java
@@ -0,0 +1,42 @@
+package jp.hazuki.yuzubrowser.legacy.utils;
+
+import android.content.Context;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.dynamiclinks.DynamicLink;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.ShortDynamicLink;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.Executor;
+
+
+public class CreateDynamicLinks {
+
+
+    private static final String domainUrlPrefix = "https://aniketbrowser.page.link";
+    private static final String appPackageName = "jp.hazuki.yuzubrowser.browser";
+
+    private DynamicLink dynamicLink;
+
+    public String getDynamicLink(String URL, String TITLE)
+    {
+
+        dynamicLink = FirebaseDynamicLinks.getInstance()
+            .createDynamicLink()
+            .setLink(Uri.parse(URL))
+            .setDomainUriPrefix(domainUrlPrefix)
+            .setAndroidParameters(new DynamicLink.AndroidParameters.Builder(appPackageName).build())
+            .setSocialMetaTagParameters(new DynamicLink.SocialMetaTagParameters.Builder().setTitle(TITLE).build())
+            .buildDynamicLink();
+
+        return dynamicLink.getUri().toString();
+    }
+
+
+}
